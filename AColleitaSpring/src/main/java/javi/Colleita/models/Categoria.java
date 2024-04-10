931a6ebd5ccb92aaed3ee57924f26ddc3e6c865a
Javi <javi1liceo@gmail.com>
2024-04-10 17:42:42 +0200

    Creación de la clase Categoría y relacionada con Producto

---
+++ AColleitaSpring/src/main/java/javi/Colleita/models/Categoria.java
@@ -0,0 +1,49 @@
+package javi.Colleita.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Categoria() {
+    }
+
+    public Categoria(Integer id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Categoria [id=" + id + ", nombre=" + nombre + "]";
+    }
+}
--- AColleitaSpring/src/main/java/javi/Colleita/models/Producto.java
+++ AColleitaSpring/src/main/java/javi/Colleita/models/Producto.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -24,13 +26,24 @@ public class Producto {
     @Column(nullable = false)
     private String direccion;
 
+    //Valoración entre 1 y 10
+    @Column(nullable = false)
+    private Integer valoracion;
+
+    //Cantidad en kg, a parte de marcar la cantidad sirve para saber si hay productos
     @Column(nullable = false)
     private Integer cantidad;
+
+    @ManyToOne
+    @JoinColumn
+    private Categoria categoria;
 
     @Column(nullable = false, length = 500)
     private String descripcion;
 
     @Column(nullable = false)
     private String imagen;
+
+    
 
 }
