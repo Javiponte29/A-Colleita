7d76a4707d753abbc62624e845e15afdbade4a78
Javi <javi1liceo@gmail.com>
2024-04-10 16:52:24 +0200

    Configuración de la clase Producto

--- AColleitaSpring/src/main/java/javi/Colleita/models/Producto.java
+++ AColleitaSpring/src/main/java/javi/Colleita/models/Producto.java
@@ -1,5 +1,31 @@
 package javi.Colleita.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "productos")
 public class Producto {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 100)
+    private String nombre;
+
+    @Column
+    private Integer precio;
+
+    @Column(nullable = false, length = 500)
+    private String descripcion;
+
+    @Column(nullable = false)
+    private String imagen;
+
+    
 }
