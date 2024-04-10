39abc2cdfd78ffac805750a253c22f29a13fa0ac
Javi <javi1liceo@gmail.com>
2024-04-10 16:27:40 +0200

    Creación de Spring para el backend

---
+++ AColleitaSpring/src/test/java/javi/Colleita/AColleitaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package javi.Colleita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AColleitaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
