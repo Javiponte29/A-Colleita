cb550367d6713f6615655563d026fb8c590e35aa
Javi <javi1liceo@gmail.com>
2024-04-10 18:00:29 +0200

    Añadiendo getters, setters, constructores y toString a la clase Producto

--- AColleitaSpring/src/main/java/javi/Colleita/models/Producto.java
+++ AColleitaSpring/src/main/java/javi/Colleita/models/Producto.java
@@ -44,6 +44,101 @@ public class Producto {
     @Column(nullable = false)
     private String imagen;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Integer precio) {
+        this.precio = precio;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public Integer getValoracion() {
+        return valoracion;
+    }
+
+    public void setValoracion(Integer valoracion) {
+        this.valoracion = valoracion;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public Producto() {
+    }
+
+    public Producto(Integer id, String nombre, Integer precio, String direccion, Integer valoracion, Integer cantidad,
+            Categoria categoria, String descripcion, String imagen) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.direccion = direccion;
+        this.valoracion = valoracion;
+        this.cantidad = cantidad;
+        this.categoria = categoria;
+        this.descripcion = descripcion;
+        this.imagen = imagen;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto [id=" + id + ", nombre=" + nombre + ", precio=" + precio + ", direccion=" + direccion
+                + ", valoracion=" + valoracion + ", cantidad=" + cantidad + ", categoria=" + categoria
+                + ", descripcion=" + descripcion + ", imagen=" + imagen + "]";
+    }
+
     
 
 }
