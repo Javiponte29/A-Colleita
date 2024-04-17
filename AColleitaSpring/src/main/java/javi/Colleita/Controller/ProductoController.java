c96b58f26e2ecf15fc0f509b0a3f5f8d475b5280
Javi <javi1liceo@gmail.com>
2024-04-17 17:53:51 +0200

    Creación del controller

---
+++ AColleitaSpring/src/main/java/javi/Colleita/Controller/ProductoController.java
@@ -0,0 +1,10 @@
+package javi.Colleita.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cont")
+public class ProductoController {
+
+}
