19fcade5826487317221f5e0a4c0a9e064e9ab41
Javi <javi1liceo@gmail.com>
2024-04-24 12:43:11 +0200

    Listar productos

--- AColleitaSpring/src/main/java/javi/Colleita/Controller/ProductoController.java
+++ AColleitaSpring/src/main/java/javi/Colleita/Controller/ProductoController.java
@@ -1,10 +1,26 @@
 package javi.Colleita.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javi.Colleita.models.Producto;
+import javi.Colleita.services.IProductoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/cont")
 public class ProductoController {
 
+    @Autowired IProductoService pSer;
+
+    @GetMapping("/list")
+    public List<Producto> get() {
+        return pSer.getProductos();
+    }
+    
 }
