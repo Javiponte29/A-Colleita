6bd829063d6fc4984a07ac9cb19de487697ccfef
Javi <javi1liceo@gmail.com>
2024-04-24 13:02:21 +0200

    Intentando hacer la página en varios idiomas

--- ColleitaReact/src/App.jsx
+++ ColleitaReact/src/App.jsx
@@ -3,7 +3,6 @@ import Home from './components/Home';
 
 function App() {
 
-
     return(
         <div>
             <Home />
--- ColleitaReact/src/main.jsx
+++ ColleitaReact/src/main.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+/*
+import { createRoot } from 'react-dom/client';
+import i18next from './i18n'
+i18next.t('my.key')
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <App />
+);*/
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
