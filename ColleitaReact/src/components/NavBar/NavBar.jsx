a0bb14455d64ebca495edc14c15c820ca227e894
Javi <javi1liceo@gmail.com>
2024-05-14 10:20:25 +0200

    Barra de navegación responsive

--- ColleitaReact/src/components/NavBar/NavBar.jsx
+++ ColleitaReact/src/components/NavBar/NavBar.jsx
@@ -16,21 +16,33 @@ const NavBar = () => {
         console.log(productos);
     }, [])
 
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
 
     return (
-<div className="flex items-center justify-around border-b-2 shadow-lg pb-4">
-    <div className='flex items-center justify-between space-x-6'>
-    <NavLink to={"/"}><img src="https://cdn-icons-png.flaticon.com/512/424/424227.png" alt="Logo" className="w-20 h-20" /></NavLink>
-    <NavLink to={"/"} className="text-green-500 font-bold text-4xl">A Colleita</NavLink>
+<div className="flex flex-col lg:flex-row items-center justify-around border-b-2 shadow-lg pb-4">
+    <div className='flex items-center space-x-6'>
+        <NavLink to={"/"}><img src="https://cdn-icons-png.flaticon.com/512/424/424227.png" alt="Logo" className="w-20 h-20" /></NavLink>
+        <NavLink to={"/"} className="text-green-500 font-bold text-4xl">A Colleita</NavLink>
     </div>
-    <nav className="space-x-20">
-        <NavLink to={""} className="font-bold text-2xl">Home</NavLink>
-        <NavLink to={""} className="font-bold text-2xl">Productos</NavLink>
-        <NavLink to={""} className="font-bold text-2xl">About us</NavLink>
-        <NavLink to={""} className="font-bold text-2xl">Login</NavLink>
+    <button className="lg:hidden" onClick={toggleMenu}>
+        <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+        </svg>
+    </button>
+    <nav className={`lg:flex lg:items-center lg:justify-around lg:space-x-20 lg:block ${isOpen ? 'block' : 'hidden'} lg:relative w-full lg:w-auto flex flex-col lg:flex-row`}>
+        <div className="font-bold text-2xl lg:text-2xl hover:text-green-500 text-center"><NavLink to={"/"}>Home</NavLink></div>
+        <div className="font-bold text-2xl lg:text-2xl hover:text-green-500 text-center"><NavLink to={""}>Productos</NavLink></div>
+        <div className="font-bold text-2xl lg:text-2xl hover:text-green-500 text-center"><NavLink to={""}>About us</NavLink></div>
+        <div className="font-bold text-2xl lg:text-2xl hover:text-green-500 text-center"><NavLink to={""}>Login</NavLink></div>
+        <div className="font-bold text-2xl lg:text-2xl hover:text-green-500 flex justify-center"><NavLink to={""}><svg width="30" height="30" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"> <path stroke="none" d="M0 0h24v24H0z"/> <circle cx="9" cy="19" r="2"/> <circle cx="17" cy="19" r="2"/> <path d="M3 3h2l2 12a3 3 0 0 0 3 2h7a3 3 0 0 0 3 -2l1 -7h-15.2"/> </svg></NavLink></div>
     </nav>
-    <NavLink to={""}><svg width="30" height="30" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"> <path stroke="none" d="M0 0h24v24H0z"/> <circle cx="9" cy="19" r="2"/> <circle cx="17" cy="19" r="2"/> <path d="M3 3h2l2 12a3 3 0 0 0 3 2h7a3 3 0 0 0 3 -2l1 -7h-15.2"/> </svg></NavLink>
 </div>
+
 
     );
 };
