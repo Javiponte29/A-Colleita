aa34fb7f11c8d746c7d5d67ee0221ece41f1dab7
Javi <javi1liceo@gmail.com>
2024-05-15 12:23:49 +0200

    Logout hecho

--- ColleitaReact/src/components/NavBar/NavBar.jsx
+++ ColleitaReact/src/components/NavBar/NavBar.jsx
@@ -1,11 +1,11 @@
 import { Link, NavLink } from 'react-router-dom';
 import './NavBar.css';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import { useAuth } from '../../context/authContext'; // Asegúrate de importar el contexto de autenticación
 
 const NavBar = () => {
-
     const [productos, setProductos] = useState([]);
+    const { user, logout } = useAuth(); // Obtén el usuario autenticado y la función de logout del contexto
 
     useEffect(() => {
         fetch(`http://localhost:8080/cont/list`)
@@ -14,7 +14,7 @@ const NavBar = () => {
             setProductos(result);
         });
         console.log(productos);
-    }, [])
+    }, []);
 
     const [isOpen, setIsOpen] = useState(false);
 
@@ -22,32 +22,41 @@ const NavBar = () => {
         setIsOpen(!isOpen);
     };
 
-
     return (
-<div className="flex flex-col lg:flex-row items-center justify-around border-b-2 shadow-lg pb-4">
-    <div className='flex items-center space-x-6'>
-        <NavLink to={"/"}><img src="https://cdn-icons-png.flaticon.com/512/424/424227.png" alt="Logo" className="w-20 h-20" /></NavLink>
-        <NavLink to={"/"} className="verde font-bold text-4xl">A Colleita</NavLink>
-    </div>
-    <button className="lg:hidden" onClick={toggleMenu}>
-        <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
-        </svg>
-    </button>
-    <nav className={`lg:flex lg:items-center lg:justify-around lg:space-x-20 lg:block ${isOpen ? 'block' : 'hidden'} lg:relative w-full lg:w-auto flex flex-col lg:flex-row`}>
-        <div className="font-bold text-2xl lg:text-2xl hover:text-green-600 text-center"><NavLink to={"/"}>Home</NavLink></div>
-        <div className="font-bold text-2xl lg:text-2xl hover:text-green-600 text-center"><NavLink to={""}>Productos</NavLink></div>
-        <div className="font-bold text-2xl lg:text-2xl hover:text-green-600 text-center"><NavLink to={""}>About us</NavLink></div>
-        <div className="font-bold text-2xl lg:text-2xl hover:text-green-600 text-center"><NavLink to={"/login"}>Login</NavLink></div>
-        <div className="font-bold text-2xl lg:text-2xl hover:text-green-600 flex justify-center"><NavLink to={""}><svg width="30" height="30" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"> <path stroke="none" d="M0 0h24v24H0z"/> <circle cx="9" cy="19" r="2"/> <circle cx="17" cy="19" r="2"/> <path d="M3 3h2l2 12a3 3 0 0 0 3 2h7a3 3 0 0 0 3 -2l1 -7h-15.2"/> </svg></NavLink></div>
-    </nav>
-</div>  
-
-
+        <div className="flex flex-col lg:flex-row items-center justify-around border-b-2 shadow-lg pb-4">
+            <div className='flex items-center space-x-6'>
+                <NavLink to={"/"}><img src="https://cdn-icons-png.flaticon.com/512/424/424227.png" alt="Logo" className="w-20 h-20" /></NavLink>
+                <NavLink to={"/"} className="verde font-bold text-4xl">A Colleita</NavLink>
+            </div>
+            <button className="lg:hidden" onClick={toggleMenu}>
+                <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+                </svg>
+            </button>
+            <nav className={`lg:flex lg:items-center lg:justify-around lg:space-x-20 lg:block ${isOpen ? 'block' : 'hidden'} lg:relative w-full lg:w-auto flex flex-col lg:flex-row`}>
+                <div className="font-bold text-2xl lg:text-2xl hover:text-green-600 text-center"><NavLink to={"/"}>Home</NavLink></div>
+                <div className="font-bold text-2xl lg:text-2xl hover:text-green-600 text-center"><NavLink to={""}>Productos</NavLink></div>
+                <div className="font-bold text-2xl lg:text-2xl hover:text-green-600 text-center"><NavLink to={""}>About us</NavLink></div>
+                {!user ? (
+                    <div className="font-bold text-2xl lg:text-2xl hover:text-green-600 text-center"><NavLink to={"/login"}>Login</NavLink></div>
+                ) : (
+                    <div className="font-bold text-2xl lg:text-2xl hover:text-green-600 text-center"><button onClick={logout}>Logout</button></div>
+                )}
+                <div className="font-bold text-2xl lg:text-2xl hover:text-green-600 flex justify-center"><NavLink to={""}>
+                    <svg width="30" height="30" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z"/>
+                        <circle cx="9" cy="19" r="2"/>
+                        <circle cx="17" cy="19" r="2"/>
+                        <path d="M3 3h2l2 12a3 3 0 0 0 3 2h7a3 3 0 0 0 3 -2l1 -7h-15.2"/>
+                    </svg>
+                </NavLink></div>
+            </nav>
+        </div>
     );
 };
 
 export default NavBar;
+
 
 /*            {productos.map(producto =>(
                     <div className='prod-home'>
