1267dd1df5537562bd23dbe2a3e7b7197ec95b4d
Javi <javi1liceo@gmail.com>
2024-05-15 13:04:03 +0200

    Añadiendo estilos

--- ColleitaReact/src/components/NavBar/NavBar.jsx
+++ ColleitaReact/src/components/NavBar/NavBar.jsx
@@ -40,7 +40,7 @@ const NavBar = () => {
                 {!user ? (
                     <div className="font-bold text-2xl lg:text-2xl hover:text-green-600 text-center"><NavLink to={"/login"}>Login</NavLink></div>
                 ) : (
-                    <div className="font-bold text-2xl lg:text-2xl hover:text-green-600 text-center"><NavLink to={"/profile"}><svg class="h-8 w-8"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <div className="font-bold text-2xl lg:text-2xl hover:text-green-600 text-center flex justify-center"><NavLink to={"/profile"}><svg class="h-8 w-8"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"/>
                     </svg>
 
--- ColleitaReact/src/components/UserProfile/UserProfile.jsx
+++ ColleitaReact/src/components/UserProfile/UserProfile.jsx
@@ -26,16 +26,16 @@ const UserProfile = () => {
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
             </svg>
           </button>
-          <h2 className="text-2xl font-bold mb-4">Perfil de Usuario</h2>
+          <h2 className="text-2xl font-bold mb-4">Perfil de {user.displayName}</h2>
           <p className="text-gray-600 mb-4">Email: {user.email}</p>
-          <button onClick={handleLogout} className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 focus:outline-none">Cerrar sesión</button>
+          <button onClick={handleLogout} className="bg-verde2 text-white px-4 py-2 flex items-center font-medium rounded focus:outline-none">Cerrar sesión <svg class="h-6 w-6 pl-1"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" />  <path d="M7 12h14l-3 -3m0 6l3 -3" /></svg></button>
         </div>
       </div>
 
       <div className="flex-1 flex flex-col">
         <header className="bg-white shadow p-4 flex justify-between items-center">
           <h1 className="text-2xl font-bold">Mi Perfil</h1>
-          <button onClick={toggleSidebar} className="bg-gray-800 text-white px-4 py-2 rounded hover:bg-gray-700 focus:outline-none">Abrir Menú</button>
+          <button onClick={toggleSidebar} className="bg-verde2 text-white px-4 py-2 rounded hover:border-green-400 focus:outline-none">Abrir Menú</button>
         </header>
 
         <main className="p-4 flex-grow">
