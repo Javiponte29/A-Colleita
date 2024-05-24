747f24e107567a7acb44030e1f63082658575a97
Javi <javi1liceo@gmail.com>
2024-05-24 17:42:45 +0200

    Acabado el footer

--- ColleitaReact/src/components/Footer/Footer.jsx
+++ ColleitaReact/src/components/Footer/Footer.jsx
@@ -4,7 +4,15 @@ const Footer = () => {
 
     return (
         <footer className='bg-gray-300'>
-            <Link to={'/'} className='text-2xl font-medium' title='Home'>Home</Link>
+            <section className='flex justify-around'>
+                <Link to={'/'} className='text-2xl font-medium' title='Home'>Home</Link>
+                <Link to={'/products'} className='text-2xl font-medium' title='Home'>Products</Link>
+                <Link to={'/aboutus'} className='text-2xl font-medium' title='Home'>About us</Link>
+            </section>
+            <section className='flex justify-around'>
+                <Link><img className='w-10 h-10' src="https://www.svgrepo.com/show/521711/instagram.svg" alt="Instagram" /></Link>
+                <Link><img className='w-14 h-10' src="https://logos-world.net/wp-content/uploads/2023/08/X-Logo.png" alt="Instagram" /></Link>
+            </section>
         </footer>
     );
 };
